df8886ab45746520b1f2b04ed6ddd69797eb3c3a
Lorraine Alvares <lorraine.alvares@gmail.com>
2021-01-10 18:00:34 +0530

    Builder Pattern

---
+++ src/creational/LunchOrder.java
@@ -0,0 +1,73 @@
+package creational;
+
+ public class LunchOrder {
+    public static class Builder{
+        private String bread;
+        private String dressing;
+        private String condiments;
+        private String vegetable;
+
+        public Builder(){}
+
+        public Builder bread(String bread){
+            this.bread = bread;
+            return this;
+        }
+
+
+        public Builder dressing(String dressing){
+            this.dressing = dressing;
+            return this;
+        }
+
+
+        public Builder condiments(String condiments){
+            this.condiments = condiments;
+            return this;
+        }
+
+
+        public Builder vegetable(String vegetable){
+            this.vegetable = vegetable;
+            return this;
+        }
+
+        public LunchOrder build(){
+            return new LunchOrder(this);
+        }
+
+    }
+
+    private final String bread;
+    private final String dressing;
+    private final String condiments;
+    private final String vegetable;
+
+    private LunchOrder(Builder builder){
+        this.bread = builder.bread;
+        this.condiments = builder.condiments;
+        this.dressing = builder.dressing;
+        this.vegetable = builder.vegetable;
+
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+
+    public String getDressing() {
+        return dressing;
+    }
+
+    public String getCondiments() {
+        return condiments;
+    }
+
+
+    public String getVegetable() {
+        return vegetable;
+    }
+
+
+}
---
+++ src/creational/LunchOrderApp.java
@@ -0,0 +1,14 @@
+package creational;
+
+public class LunchOrderApp {
+    public static void main(String[] args){
+        LunchOrder.Builder builder = new LunchOrder.Builder();
+        LunchOrder lunchOrder = builder.bread("Brown Bread").condiments("Pepper").dressing("Mustard").
+                vegetable("Tomato").build();
+
+        System.out.println(lunchOrder.getBread());
+        System.out.println(lunchOrder.getDressing());
+        System.out.println(lunchOrder.getCondiments());
+        System.out.println(lunchOrder.getVegetable());
+    }
+}
