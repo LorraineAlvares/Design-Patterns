11bcb370183da01af4331b889a8f2adb7992d0ab
Lorraine Alvares <lorraine.alvares@gmail.com>
2021-01-10 12:50:45 +0530

    Singleton Pattern

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,11 @@
+import creational.Singleton;
+
 public class MainApp {
     public static void main(String[] args){
-        
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton);
+
+        Singleton another = Singleton.getInstance();
+        System.out.println(another);
     }
 }
--- src/creational/Singleton.java
+++ src/creational/Singleton.java
@@ -1,4 +1,14 @@
 package creational;
 
 public class Singleton {
+    private static Singleton instance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
 }
